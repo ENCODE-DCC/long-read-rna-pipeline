5b8f34601c2b3e656447e88697a5cc32eb4985aa
ottojolanki <ojolanki@stanford.edu>
2020-06-05 01:02:55 -0700

    run splice junction test with caper

--- test/caper_run.sh
+++ test/caper_run.sh
@@ -5,9 +5,9 @@ set -e
 echo "Running cromwell $CROMWELL, womtool $WOMTOOL and image $TAG"
 
 if [[ $# -eq 1 ]]; then
-    caper run $1 --docker $TAG --cromwell $CROMWELL --womtool $WOMTOOL --options ./tests/cromwell_options.json
+    caper run $1 --docker $TAG --cromwell $CROMWELL --womtool $WOMTOOL --options ./test/cromwell_options.json -m metadata.json
 fi
 
 if [[ $# -eq 2 ]]; then
-    caper run $1 --docker $TAG --cromwell $CROMWELL --womtool $WOMTOOL -i $2 --options ./tests/cromwell_options.json
+    caper run $1 --docker $TAG --cromwell $CROMWELL --womtool $WOMTOOL -i $2 --options ./test/cromwell_options.json -m metadata.json
 fi
